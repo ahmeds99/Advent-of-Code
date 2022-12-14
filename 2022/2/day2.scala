0eb625d2a85b8ac80465bf3d4ce853cc8cffa4e3
Ahmed <ahmedws@uio.no>
2022-12-14 22:19:51 +0100

    Add part2 of day 2, change same file as part1

--- 2022/2/day2.scala
+++ 2022/2/day2.scala
@@ -1,26 +1,28 @@
 import scala.io.Source._
 
+// X loss, Y draw and Z win
 @main def day2() = 
     val source = fromFile("input.txt")
     val lines = try source.mkString finally source.close()    
     
-    val shapePoint = Map("X" -> 1, "Y" -> 2, "Z" -> 3)
+    val shapePoint = Map("A" -> 1, "B" -> 2, "C" -> 3)
     val scores = Map("X" -> "rock", "A" -> "rock",
                      "Y" -> "paper", "B" -> "paper",
                      "Z" -> "scissor", "C" -> "scissor")
     var mySum: Int = 0
     for (line <- lines.split("\n")) {
         val info = line.strip().split(" ")
-        val result = decideGame(scores.getOrElse(info(0), ""), scores.getOrElse(info(1), ""))
-        mySum += shapePoint.getOrElse(info(1), 0) + result
+        if (info(1).equals("Y"))
+            mySum += shapePoint.getOrElse(info(0), 0) + 3
+        else 
+            val result = decideGame(scores.getOrElse(info(0), ""), info(1))
+            mySum += result
     }
     println(mySum)
 
 def decideGame(opponent: String, me: String): Int =
-    if (opponent.equals(me)) return 3
-
     return opponent match {
-        case "rock" => if (me.equals("paper")) 6 else 0
-        case "paper" => if (me.equals("scissor")) 6 else 0
-        case "scissor" => if (me.equals("rock")) 6 else 0
+        case "rock" => if (me.equals("X")) 3 else (2 + 6)
+        case "paper" => if (me.equals("X")) 1 else (3 + 6)
+        case "scissor" => if (me.equals("X")) 2 else (1 + 6)
     }
