fc5f98ddde065e67ac970bac9125da3e1e4dd4bd
ahmeds99 <ahmed99@live.no>
2024-12-02 18:46:43 +0100

    add part2

--- 2024/src/main/scala/Day1/Part2.scala
+++ 2024/src/main/scala/Day1/Part2.scala
@@ -1,12 +1,15 @@
 package Day1
 
-@main def part2(): Unit =
-  val file = scala.io.Source.fromFile("src/main/scala/Day1/test2.txt")
+
+@main def main(): Unit =
+  val file = scala.io.Source.fromFile("src/main/scala/Day1/input.txt")
   val lines = try file.mkString finally file.close()
 
   val allPairs = lines.split("\n").map(pair => pair.split(" {3}"))
 
   val firstList = allPairs.map(arr => arr.apply(0).toInt)
-  val secondList = allPairs.map(arr => arr.apply(1).toInt)
-  
-  println("hei")
+  val mapOfSecondListElementsCount = allPairs.map(arr => arr.apply(1).toInt).groupBy(identity).map(x => (x._1, x._2.length))
+
+  val similarityList = firstList.map(i => mapOfSecondListElementsCount.getOrElse(i, 0) * i)
+
+  println(similarityList.sum())
