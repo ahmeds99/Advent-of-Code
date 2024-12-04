9d187b71de378cdf80f53623554a78eb5a621c63
ahmeds99 <ahmed99@live.no>
2024-12-04 12:13:26 +0100

    day 2 part 1

---
+++ 2024/src/main/scala/Day2/Day2.scala
@@ -0,0 +1,23 @@
+package Day2
+
+@main def main(): Unit =
+  val file = scala.io.Source.fromFile("src/main/scala/Day2/input.txt")
+  val lines = try file.mkString finally file.close()
+
+  val allLines = lines.split("\n").map(line => line.split(" ").map(numStr => numStr.toInt))
+
+  val result = allLines.filter(report => {
+    val increasing = report(0) <= report(1)
+
+    report.sliding(2).filter(list =>
+      checkIfSorted(increasing, list(0), list(1)) && validDifference(list(0), list(1))
+    ).toList.length == report.length - 1
+  })
+  
+  println(result.length)
+
+
+def checkIfSorted(increasing: Boolean, num1: Int, num2: Int): Boolean =
+  if (increasing) num1 <= num2 else num1 >= num2
+
+def validDifference(num1: Int, num2: Int): Boolean = (num1 - num2).abs >= 1 && (num1 - num2).abs <= 3
